ac29eb47820c54505c068b2bab53e30d30c43e9c
emasurf85 <emanuele.magliozzi@gmail.com>
2016-04-22 15:14:21 +1000

    Still playing with the User testing

--- test/concepts/user/operation_test.rb
+++ test/concepts/user/operation_test.rb
@@ -12,17 +12,30 @@ class UserOperationTest < MiniTest::Spec
   end
 
   it "unique email and phone" do
-    op = User::Create.(user: { firstname: "Ema", lastname: "Maglio", gender: "Male", email: "my@email.com", phone: "0410123456" })
-    res,op = User::Create.run(user: { firstname: "Ema", lastname: "Maglio", gender: "Male", email: "my@email.com", phone: "0410123456" })
+    res,op = User::Create.run(user: { firstname: "Ema", lastname: "Maglio", gender: "Male", email: "my@email.com", phone: "04101000001" })
     res.must_equal false
-    op.errors.to_s.must_equal "{:phone=>[\"has already been taken\"], :email=>[\"has already been taken\"]}"
+    op.errors.to_s.must_equal "{:email=>[\"has already been taken\"]}"
+    res,op = User::Create.run(user: { firstname: "Ema", lastname: "Maglio", gender: "Male", email: "my1@email.com", phone: "0410123456" })
+    res.must_equal false
+    op.errors.to_s.must_equal ":phone=>[\"has already been taken\"]}"
   end
 
   it "Fails" do
-    res,op = User::Create.run(user: { firstname: "asd", lastname: "", gender: "asd", email: "asd", phone: "asd" })
+    res,op = User::Create.run(user: { firstname: "", lastname: "Maglio", gender: "Male", email: "my2@email.com", phone: "0410000010" })
     res.must_equal false
     op.errors.to_s.must_equal "{:firstname=>[\"can't be blank\"]}"
+    res,op = User::Create.run(user: { firstname: "Ema", lastname: "", gender: "Male", email: "my3@email.com", phone: "0410000100" })
+    res.must_equal false
+    op.errors.to_s.must_equal "{:lastname=>[\"can't be blank\"]}"
+    res,op = User::Create.run(user: { firstname: "Ema", lastname: "Maglio", gender: "", email: "my4@email.com", phone: "0410001000" })
+    res.must_equal false
+    op.errors.to_s.must_equal "{:gender=>[\"can't be blank\"]}"
+    res,op = User::Create.run(user: { firstname: "ema", lastname: "Maglio", gender: "Male", email: "", phone: "04100100000" })
+    res.must_equal false
+    op.errors.to_s.must_equal "{:email=>[\"can't be blank\"]}"
+    res,op = User::Create.run(user: { firstname: "Ema", lastname: "Maglio", gender: "Male", email: "my5@email.com", phone: "" })
+    res.must_equal false
+    op.errors.to_s.must_equal "{:phone=>[\"can't be blank\"]}"
   end
 
 end
- 
