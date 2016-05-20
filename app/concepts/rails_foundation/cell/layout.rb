882b7cfbc0db6af278ef6bb0c65fe1ad7fb7723d
emasurf85 <emanuele.magliozzi@gmail.com>
2016-05-20 19:23:28 +1000

    Tiny (probably not usefull modification) modification

--- app/concepts/rails_foundation/cell/layout.rb
+++ app/concepts/rails_foundation/cell/layout.rb
@@ -1,8 +1,29 @@
 module RailsFoundation
   module Cell
-    class Layout < Trailblazer::Cell
+    class Layout < ::Cell::Concept
       include ActionView::Helpers::CsrfHelper
       # include ActionController::RequestForgeryProtection
+      property :current_user
+      property :real_user
+      property :signed_in?
+
+      def show
+        render
+      end
+
+    private
+      def links
+        render
+      end
+
+      def welcome_signed_in
+        link_to("#{impersonate_icon} Hi, #{current_user.email}".html_safe, user_path(current_user))
+      end
+
+      def impersonate_icon
+        return unless real_user
+        "<i data-tooltip class=\"fi-sheriff-badge\" title=\"You really are: #{real_user.email}\"></i>"
+      end
     end
   end
 end
