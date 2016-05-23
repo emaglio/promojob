15b403d69e6234fd619428868a5b4ac1701b85ab
Nick Sutterer <apotonick@gmail.com>
2016-05-23 20:04:03 +1000

    add seeds file with admin and the very very very very very very secret password.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# encoding: UTF-8
+
+admin = User::Create.(user: { firstname: "CJ", lastname: "Kühn", email: "info@cj-agency.de", phone: "0123456789", gender: "f", password: "Test1234", confirm_password: "Test1234" }).model
+
