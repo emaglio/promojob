200487c319f73d5eee362ba28f256a63adef2bca
emasurf85 <emanuele.magliozzi@gmail.com>
2016-06-10 16:01:22 +1000

    adding search logic

--- app/concepts/job/operation/search.rb
+++ app/concepts/job/operation/search.rb
@@ -2,7 +2,15 @@ class Job < ActiveRecord::Base
   class Search < Trailblazer::Operation
     policy Session::Policy, :true?
 
-    
+    def model!(params)
+
+
+
+    end
+
+    def keyword
+      Jobs.where("title.include?")
+    end
 
   end
 end
