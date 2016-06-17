f1a024a6c11af4b0b1ff922bf4da52e56775d424
emasurf85 <emanuele.magliozzi@gmail.com>
2016-06-17 14:32:15 +1000

    fix issue with admin_for in test_help

--- test/concepts/job/job_application_test.rb
+++ test/concepts/job/job_application_test.rb
@@ -4,7 +4,8 @@ require 'test_helper'
 class JobApplicationTest < MiniTest::Spec
 
   let (:user) {User::Create.(user: attributes_for(:user)).model}
-  let (:job) {Job::Create.(job: attributes_for(:job), current_user: admin_for).model}
+  let (:job) {Job::Create.(job: attributes_for(:job), current_user: admin).model}
+  let (:admin) {admin_for}
 
   it "validates correct input" do  
     op = Job::Apply.({ user_id: user.id, job_id: job.id, message: "This is great", status: "Applied", current_user: user})
@@ -31,9 +32,9 @@ class JobApplicationTest < MiniTest::Spec
 
   it "only admin can Hire/Reject a job_application" do
     #TODO change admin_for in order to use more time in the same test
-    user = User::Create.(user: attributes_for(:user)).model
-    admin = User::Create.(user: attributes_for(:user, email: "info@cj-agency.de", phone: "98")).model
-    job = Job::Create.(job: attributes_for(:job), current_user: admin).model
+    # user = User::Create.(user: attributes_for(:user)).model
+    # admin = User::Create.(user: attributes_for(:user, email: "info@cj-agency.de", phone: "98")).model
+    # job = Job::Create.(job: attributes_for(:job), current_user: admin).model
 
     op = Job::Apply.({ user_id: user.id, job_id: job.id, message: "This is great", status: "Applied", current_user: user})
     op.model.persisted?.must_equal true
