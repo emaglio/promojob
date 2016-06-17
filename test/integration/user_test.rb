d6bb8a042aa55244efa34ef10f0fd0038384fa6d
emasurf85 <emanuele.magliozzi@gmail.com>
2016-06-17 17:43:27 +1000

    Added integration test for editing user

--- test/integration/user_test.rb
+++ test/integration/user_test.rb
@@ -35,7 +35,36 @@ class UserIntegrationTest < Trailblazer::Test::Integration
 
     page.must_have_content "All Jobs"   #TODO: change to Hi, name when we implement that
                                         #after creating a user i'm signed in as well
+    page.must_have_content "MyName" #flash    
+  end
 
+  it "only user can edit user" do
+    log_in_as_user #firstname= Ema email= my@email.com password= Test1
+
+    page.must_have_link "Hi, Ema"
+    click_link "Hi, Ema"
+    page.must_have_link "Edit"
+    click_link 'Edit'
+
+    page.must_have_css "#user_firstname"
+    page.must_have_css "#user_lastname"
+    # page.must_have_css "#user_gender" TODO: how to test this?
+    page.must_have_css "#user_email"
+    page.must_have_css "#user_phone"
+    page.must_have_css "#user_password"
+    page.must_have_css "#user_confirm_password"
+
+    user = User.find_by(email: "my@email.com")
+
+    within("//form[@id='edit_user_#{user.id}']") do
+      fill_in 'Firstname', with: "EmaNew"
+      fill_in 'Password', with: "Test1"
+      fill_in 'Confirm Password', with: "Test1"
+    end 
+    click_button "Update User"
+
+    page.must_have_content "Your profile has been updated" #flash
+    page.must_have_content "Hi, EmaNew"
   end
 
   it "successfully delete user" do
@@ -47,6 +76,7 @@ class UserIntegrationTest < Trailblazer::Test::Integration
     page.must_have_link "Delete"
 
     click_link "Delete"
+    page.must_have_content "User and Job Applications related have been deleted" #flash
     page.must_have_content "All Jobs"
 
     visit "sessions/new"
