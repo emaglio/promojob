f2fcf13ccd07a5788b5358ff48d2e6868336c85f
emasurf85 <emanuele.magliozzi@gmail.com>
2016-06-17 15:29:50 +1000

    Test delete job/user --> delete jobApp (only operation) close #12

--- test/concepts/job/job_application_test.rb
+++ test/concepts/job/job_application_test.rb
@@ -4,7 +4,9 @@ require 'test_helper'
 class JobApplicationTest < MiniTest::Spec
 
   let (:user) {User::Create.(user: attributes_for(:user)).model}
+  let (:user2) {User::Create.(user: attributes_for(:user, email: "test@mail.com", phone: "00")).model}
   let (:job) {Job::Create.(job: attributes_for(:job), current_user: admin).model}
+  let (:job2) {Job::Create.(job: attributes_for(:job, title: "Web Dev"), current_user: admin).model}
   let (:admin) {admin_for}
 
   it "validates correct input" do  
@@ -48,6 +50,37 @@ class JobApplicationTest < MiniTest::Spec
     jobApp.user_id.must_equal user.id
     jobApp.job_id.must_equal job.id
     jobApp.status.must_equal "Hire"
+  end
 
+  it "delete jobApp after delete user" do
+    #same user applies for the same job 
+    op = Job::Apply.({ user_id: user.id, job_id: job.id, message: "This is great", status: "Applied", current_user: user})
+    op.model.persisted?.must_equal true
+    op = Job::Apply.({ user_id: user.id, job_id: job2.id, message: "This is great2", status: "Applied", current_user: user})
+    op.model.persisted?.must_equal true
+
+    jobApps = JobApplication.where("user_id = ?", user.id)
+    jobApps.size.must_equal 2
+
+    op = User::Delete.({id: user.id, current_user: user})
+
+    jobApps = JobApplication.where("user_id = ?", user.id)
+    jobApps.size.must_equal 0
+  end
+
+  it "delete jobApp after delete job" do 
+    #same job for 2 different users
+    op = Job::Apply.({ user_id: user.id, job_id: job.id, message: "This is great", status: "Applied", current_user: user})
+    op.model.persisted?.must_equal true
+    op = Job::Apply.({ user_id: user2.id, job_id: job.id, message: "This is great2", status: "Applied", current_user: user})
+    op.model.persisted?.must_equal true
+
+    jobApps = JobApplication.where("job_id = ?", job.id)
+    jobApps.size.must_equal 2
+
+    op = Job::Delete.({id: job.id, current_user: admin})
+
+    jobApps = JobApplication.where("job_id = ?", job.id)
+    jobApps.size.must_equal 0
   end
 end
