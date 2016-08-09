07bec097e1aa9626d4d20eb8e14668f200af2949
emasurf85 <emanuele.magliozzi@gmail.com>
2016-08-09 18:18:42 +1000

    Added number of job_application for each status in the navigation bar

--- app/concepts/rails_foundation/cell/navigation.rb
+++ app/concepts/rails_foundation/cell/navigation.rb
@@ -5,6 +5,21 @@ module RailsFoundation
       def tyrant
         context[:tyrant]
       end
+      
+      def num_apply
+        ::JobApplication.where("status == ?", "Apply").size
+      end
+
+      def num_hire
+        ::JobApplication.where("status == ?", "Hire").size
+      end
+
+      def num_reject
+        ::JobApplication.where("status == ?", "Reject").size
+      end
+
     end
+
+
   end
 end
